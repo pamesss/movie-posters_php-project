22347f95f5746fcc0a52d8600fb45de426a5f8a6
pamesss <114709755+pamesss@users.noreply.github.com>
2023-05-08 03:10:28 -0300

    renamed the index.php for the website to be able to load

--- page-component__navbar.php
+++ page-component__navbar.php
@@ -1,9 +1,9 @@
 <aside >
     <ul class="navbar__box navbar__toggle">
-        <a href="_index__home-page.php" target="_parent"><h2 class="navbar__list-title"><b>MOVIE</b> POSTERS</h2></a>
+        <a href="index.php" target="_parent"><h2 class="navbar__list-title"><b>MOVIE</b> POSTERS</h2></a>
 
         <!--the .selected__page class to highlight the selected page on the menu must be added via javascript-->
-        <a href="_index__home-page.php" target="_parent"><li class="navbar__list-option">Home</li></a>
+        <a href="index.php" target="_parent"><li class="navbar__list-option">Home</li></a>
         <a href="category__action-page.php" target="_self"><li class="navbar__list-option">Action</li></a>
         <a href="category__animation-page.php" target="_self"><li class="navbar__list-option">Animation</li></a>
         <a href="category__comedy-page.php" target="_self"><li class="navbar__list-option">Comedy</li></a>
